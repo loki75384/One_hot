4ec740919f0d084bee215c8ddb06b33ba4c47990
loki47 <larsentuta49@gmail.com>
2023-12-05 02:23:00 +0300

    .

---
+++ 044.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import random
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+
+one_hot_data = pd.get_dummies(data['whoAmI'])
+data.drop(columns=['whoAmI'], inplace=True)
+data = pd.concat([data, one_hot_data], axis=1)
+
+data.head()
+
+
